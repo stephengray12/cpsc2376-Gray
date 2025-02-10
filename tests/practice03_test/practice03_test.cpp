6a7f42728b3ec2b9bdebce22e8c76435ca36e06c
Stephen Gray <97068845+stephengray12@users.noreply.github.com>
2025-02-09 19:27:53 -0600

    Added Native Unit Test for Practice 03.

---
+++ tests/practice03_test/practice03_test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace practice03test
+{
+	TEST_CLASS(practice03test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
